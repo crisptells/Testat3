983f3859b7dc9229e927b577f3fd9e3199b38b2a
Chrisi43 <85624831+Chrisi43@users.noreply.github.com>
2022-01-31 15:00:18 +0100

    Update Worker.java

--- src/Aufgabe14/Worker.java
+++ src/Aufgabe14/Worker.java
@@ -6,29 +6,31 @@ import java.net.DatagramSocket;
 import java.util.LinkedList;
 
 public class Worker extends Thread{
-	private int id;
-	private byte[] buf;
-	private DatagramSocket server;
-	private RequestQueue queue;
-	private FileMonitor monitor;
-	
-	public Worker(int id, RequestQueue q, DatagramSocket server, FileMonitor m) {
-		this.id = id;
-		this.queue = q;
-		this.server = server;
-	}
-	
-	@Override
-	public void run() {
-		while (true) {
-			DatagramPacket packet = queue.getNext();
-			System.out.println("Worker: " + id + " started working");
-			DatagramPacket sendPacket = MyFile.process(packet, monitor);
-			try {
-				server.send(sendPacket);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    private int id;
+    private byte[] buf;
+    private DatagramSocket server;
+    private RequestQueue queue;
+
+    // Konstruktor für Worker-Klasse
+    public Worker(int id, RequestQueue q, DatagramSocket server) {
+        this.id = id;
+        this.queue = q;
+        this.server = server;
+    }
+
+    // wird am Anfang gestartet und holt sich solange das nächste Packet aus der Warteschlange
+    // bis es keins mehr gibt und bearbeitet diese
+    @Override
+    public void run() {
+        while (true) {
+            DatagramPacket packet = queue.getNext();
+            System.out.println("Worker: " + id + " started working");
+            DatagramPacket sendPacket = MyFile.process(packet);
+            try {
+                server.send(sendPacket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
