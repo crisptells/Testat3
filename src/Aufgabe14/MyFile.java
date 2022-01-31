d201447b81dc53214f2737d35ad6dfc7f609f3b5
I539230 <I539230@W-PF27KEWP>
2022-01-31 17:51:36 +0100

    Kommentare

--- src/Aufgabe14/MyFile.java
+++ src/Aufgabe14/MyFile.java
@@ -56,7 +56,7 @@ public class MyFile {
 	        File myObj = new File("C:/Users/"+userName+"/Desktop/AdvIT14/"+subArray[0]+".txt");
 	        monitor = monitors.get(subArray[0]);
 	        Scanner myReader;
-	        //Starten der Monitormethode zum lesen
+	        //Starten der Monitormethode lesen
 	        monitor.startRead();
 			try {
 				//Simulieren einer längeren Bearbeitungszeit
@@ -65,17 +65,21 @@ public class MyFile {
 				myReader = new Scanner(myObj);
 				String[] data = new String[255];
 				int i = 1;
+				//Auslesen der Datei zeilenweise in die einzelnen Felder des Arrays
 		        while (myReader.hasNextLine()) {
 		        	data[i] = myReader.nextLine();
 		        	i++;
 		        }
 		        myReader.close();
+		        //Umwandeln der Zeilennummer von String zu Int
 		        String line_string = subArray[1];
 		        String line = line_string.trim();
 		        int line_no = Integer.parseInt(line);
-		        System.out.println(data[line_no]);
+		        //Zugriff auf die Zeile mit der entsprechenden Zeilennummer
 		        String line_data = data[line_no];
+		        //Austritt aus der Monitormethode zum lesen
 		        monitor.stopRead();
+		        //Erstellen und Zurückgeben des Packets mit der Ausgelesenen Zeile
 		        DatagramPacket sendPacket = new DatagramPacket(line_data.getBytes(), line_data.getBytes().length, packet.getAddress(), packet.getPort());
 		        return sendPacket;
 			} catch (FileNotFoundException e) {
@@ -88,69 +92,71 @@ public class MyFile {
 	}
 	
 	public DatagramPacket write(DatagramPacket packet) {
+		//Auslesen des Befehls aus dem Packet
 		String content = readPacket(packet);
 		String answer = "Error";
+		//Aufteilen des Befehls in Anweisung (READ oder WRITE) und Inhalt
 		String[] contentArray = content.split(" ", 2);
+		//Aufteilen des Inhalts in Dateiname, Zeilennummer und Text
 		String[] subArray = contentArray[1].split(",");
 		System.out.println("ist im write:)");
 		if (contentArray.length != 2) {return null;}
 		if (subArray.length != 3) {return null;}
 		if (contentArray[0].equals("WRITE")) {
-			System.out.println("hat befehl erkannt und syntax richtig");
 			String fileName = subArray[0];
 			String line_no = subArray[1];
 			String contents = subArray[2];
 			//Datei mit dem Namen des Keys lokalisieren
 			String userName = System.getProperty("user.name");
 	        File myObj = new File("C:/Users/"+userName+"/Desktop/AdvIT14/"+fileName+".txt");
+	        //Den Monitor anhand des Dateinamens auslesen
 	        monitor = monitors.get(fileName);
-	        System.out.println("hat monitor gefunden");
 	        String[] data = null;
 	        Scanner myReader;
 	        Scanner lineCounter;
-	        System.out.println("kurz vor monitor.startRead");
+	        //Starten der Monitormethode lesen, da hier auch ungestört gelesen werden kann 
 	        monitor.startWrite();
 			try {
+				//Zählen der Zeilen
 				lineCounter = new Scanner(myObj);
 				int lines = 0;
 				while (lineCounter.hasNextLine()) {
 		        	lines++;
 		        	lineCounter.nextLine();
 		        }
-				System.out.println("Lines: " + lines);
+				//Initialisieren eines Arrays, das genau so lang ist wie Zeilen gezählt wurden
 				data = new String[lines];
 				lineCounter.close();
 				
 				int i = 0;
+				//Auslesen der Datei in die Zeilen des zuvor initialisierten Arrays
 				myReader = new Scanner(myObj);
 		        while (myReader.hasNextLine()) {
 		        	data[i] = myReader.nextLine();
 		        	i++;
 		        }
 		        myReader.close();
-		        System.out.println("hat gelesen");
 			} catch (FileNotFoundException e) {
 				return new DatagramPacket(answer.getBytes(), answer.getBytes().length, packet.getAddress(), packet.getPort());
 			}
 			
-			 System.out.println("nach stopRead");
 			//Ändern der gegebenen Zeile
 			int line_noInt = Integer.parseInt(line_no.trim());
 			data[line_noInt-1] = contents.trim();
 			
 			//Überschreiben der gegebenen Datei
-			
 			try {
 				//Simulieren einer längeren Bearbeitungszeit
 				Thread.sleep(2000);
+				//Überschreiben der Datei mithilfe eines PrintWriters
 				PrintWriter fileOut = new PrintWriter(myObj);
 				for(String s: data) {
 					fileOut.println(s);
 				}
 				fileOut.close();
-				answer = "Hat geklappt!";
+				//Austritt aus der Monitormethode zum schreiben
 				monitor.stopWrite();
-				 System.out.println("hat geklappt (Server)");
+				//Zurückgeben des Antwortpackets
 				return new DatagramPacket(answer.getBytes(), answer.getBytes().length, packet.getAddress(), packet.getPort());
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -159,6 +165,7 @@ public class MyFile {
 			}
 			
 		}
+		//Im Fall, dass der Befehl nicht erkannt wurde, wird ein Packet zurückgeschickt, dass Error enthält
 		return new DatagramPacket(answer.getBytes(), answer.getBytes().length, packet.getAddress(), packet.getPort());
 	}
 }
