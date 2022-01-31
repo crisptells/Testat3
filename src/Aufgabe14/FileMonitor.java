82b3546f514e833fbb6a652628c2f86320376ae1
I539230 <I539230@W-PF27KEWP>
2022-01-31 14:02:21 +0100

    Monitor :,()

---
+++ src/Aufgabe14/FileMonitor.java
@@ -0,0 +1,44 @@
+package Aufgabe14;
+
+public class FileMonitor {
+	
+	boolean activeWriter = false;
+	int readerCnt = 0;
+	int writerCnt = 0;
+	
+	public synchronized void startRead() {
+		while (writerCnt > 0) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		readerCnt++;
+		notifyAll();
+	}
+	
+	public synchronized void stopRead() {
+		notifyAll();
+		readerCnt--;
+	}
+	
+	public synchronized void startWrite() {
+		writerCnt++;
+		while ((readerCnt > 0) || (activeWriter)) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		activeWriter = true;
+	}
+	
+	public synchronized void stopWrite() {
+		activeWriter = false;
+		writerCnt--;
+		notifyAll();
+	}
+	
+}
