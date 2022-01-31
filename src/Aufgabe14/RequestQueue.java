1e0eed404a1a8bb4188ec5ff39846802da0e46cf
Chrisi43 <85624831+Chrisi43@users.noreply.github.com>
2022-01-31 14:56:20 +0100

    Update RequestQueue.java

--- src/Aufgabe14/RequestQueue.java
+++ src/Aufgabe14/RequestQueue.java
@@ -5,31 +5,36 @@ import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
 public class RequestQueue {
-	
-	private LinkedList<DatagramPacket> queue = new LinkedList<DatagramPacket>();
-	
-	synchronized void add(DatagramPacket packet) {
-		queue.add(packet);
-		this.notify();
-	}
-	
-	synchronized DatagramPacket getNext() {
-		DatagramPacket returningPacket = null;
-		while(queue.size() == 0) {
-			try {
-				this.wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		returningPacket = queue.pop();
-		System.out.println("Element gefunden:)");
-		return returningPacket;
-	}
-	
-	public int size() {
-		return queue.size();
-	}
+
+    private LinkedList<DatagramPacket> queue = new LinkedList<DatagramPacket>();
+
+    // Fügt der Warteschlange ein Packet hinzu und benachrichtig alle
+    synchronized void add(DatagramPacket packet) {
+        queue.add(packet);
+        this.notify();
+    }
+
+    // fordert das nächste Packet in der Warteschlange
+    synchronized DatagramPacket getNext() {
+        // wird an der Warteschlange gewartet wenn es kein Packet gibt
+        DatagramPacket returningPacket = null;
+        while(queue.size() == 0) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // gibt das Packet an der ersten Stelle aus und löscht es danach
+        returningPacket = queue.pop();
+        System.out.println("Element gefunden:)");
+        return returningPacket;
+    }
+
+    // gibt die Größe der Warteschlange
+    public int size() {
+        return queue.size();
+    }
 
 }
